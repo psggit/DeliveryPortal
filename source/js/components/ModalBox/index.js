d06a3d3ef32477af9e5b856c2994090c3eb7a418
Madhur Garg <madhurgarg@Madhurs-MacBook-Pro.local>
2017-07-28 17:03:13 +0530

    added modal close support for ESC key and document click

--- source/js/components/ModalBox/index.js
+++ source/js/components/ModalBox/index.js
@@ -1,7 +1,35 @@
 import React from 'react'
 import './index.scss'
+import { unmountComponentAtNode } from 'react-dom'
 
 class ModalBox extends React.Component {
+  handleClick(e) {
+    if (e.keyCode == 27) {
+      unmountComponentAtNode(document.getElementById('confirm-modal'))
+      if (document.querySelector('.newFeature') !== null) {
+        document.querySelector('.newFeature').className = 'sm-group2 smg2-1'
+      }
+
+      document.body.setAttribute('class', '')
+      localStorage.setItem('isGotscheduleNewFeature', true)
+    }
+  }
+
+  handlePress(e) {
+      if (e.target.className === 'modal-overlay') {
+        unmountComponentAtNode(document.getElementById('confirm-modal'))
+        document.body.setAttribute('class', '')
+      }
+  }
+
+  componentDidMount() {
+    window.addEventListener('keyup', this.handleClick)
+    document.addEventListener('click', this.handlePress)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this.handleClick)
+    document.removeEventListener('click', this.handlePress)
+  }
   render () {
     return (
       <div className='modal-overlay'>
