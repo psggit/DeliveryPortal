8f0d7e35db507f203d8efcd2cbd1974d81045a74
Madhur Garg <madhurgarg@Madhurs-MacBook-Pro.local>
2017-07-31 16:49:07 +0530

    extended base modalbox to confirmation modal

---
+++ source/js/components/ModalBox/ConfirmModal.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ModalHeader from './ModalHeader'
+import ModalFooter from './ModalFooter'
+import ModalBody from './ModalBody'
+import ModalBox from './'
+import { unMountModal } from './utils'
+
+export default function confirmModal (data) {
+  return class confirmModal extends React.Component {
+    constructor (props) {
+      super(props)
+      this.state = {
+
+      }
+    }
+    render () {
+      return (
+        <ModalBox>
+          <ModalHeader>Modal heading</ModalHeader>
+            <ModalBody>{ data.confirmMessage }</ModalBody>
+          <ModalFooter>
+            <button className='btn btn-secondary' onClick={unMountModal}>Cancel</button>
+            <button className='btn btn-primary' onClick={data.handleConfirmAssign}>Confirm</button>
+          </ModalFooter>
+        </ModalBox>
+      )
+    }
+  }
+}
