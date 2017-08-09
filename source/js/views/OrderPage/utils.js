1a37e855fc6520b0c88a07d3caabc3112ac69ae6
BG <BG@Admins-MacBook-Pro-2.local>
2017-08-09 15:50:26 +0530

    added strict number check utility

--- source/js/views/OrderPage/utils.js
+++ source/js/views/OrderPage/utils.js
@@ -42,3 +42,17 @@ export function getTimeDiff(d) {
     (dateNow - dateTarget) / 60000
   )
 }
+
+export function validateNumType(keyCode) {
+  let allowed = [ 8, 46, 37, 39, 9 ]
+  return allowed.indexOf(keyCode) > -1 || (keyCode >=48 && keyCode <=57)
+}
+
+export function checkCtrlA(e) {
+  if (e.ctrlKey) {
+    if (e.keyCode == 65 || e.keyCode == 97) {
+      return true
+    }
+  }
+  return false
+}
